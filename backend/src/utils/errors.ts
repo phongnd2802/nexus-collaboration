9df696ae183b223e6b6a41f43a46960980fa8585
giakhang777 <nguyengiakhang116@gmail.com>
2025-11-05 22:53:50 +0700

    khang - update type error, invitation done

--- backend/src/utils/errors.ts
+++ backend/src/utils/errors.ts
@@ -2,11 +2,14 @@ export class AppError extends Error {
   status: number;
   code: string;
 
-  constructor(status: number, code: string, message: string) {
+  constructor(status: number, code: string, message: string, stack?: any) {
     super(message);
     this.name = 'AppError';
     this.status = status;
     this.code = code;
+    if (stack) {
+      this.stack = stack;
+    }
   }
 }
 
